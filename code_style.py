c551ffea3367b5204f348121fb579e3d11219c79
karandesai-96 <karandesai_96@live.com>
2016-01-18 23:18:08 +0530

    Added two new methods to code_style.py
    New methods are to preview refactored file, and do actual refactor
    of the file passed as parameter.

--- code_style.py
+++ code_style.py
@@ -6,16 +6,39 @@ arg_parser = argparse.ArgumentParser(description="Align coding style \
             according to PEP8 guidelines.")
 
 arg_parser.add_argument("-c", "--check", action="store_true")
+arg_parser.add_argument("-p", "--preview", action="store_true")
+arg_parser.add_argument("-r", "--refactor", action="store_true")
 arg_parser.add_argument("-f", "--file")
 
 
-def check_pep8_errors(f):
-    os.system("pep8 " + python_file)
+def check_pep8_errors(filename):
+    print "***** PEP8 errors in " + filename.split("/")[2] + " *****"
+    os.system("pep8 " + filename)
+    print "DONE !\n\n"
+
+
+def preview_autopep8(filename):
+    print "***** After refactoring " + filename.split("/")[2] + " *****"
+    os.system("autopep8 " + filename)
+    print "DONE !\n\n"
+
+
+def refactor_autopep8(filename):
+    print "***** Refactoring " + filename.split("/")[2] + " *****"
+    os.system("autopep8 -i -a " + filename)
+    print "DONE !\n\n"
 
 
 if __name__ == '__main__':
     arguments = vars(arg_parser.parse_args(sys.argv[1:]))
-    python_file = "minecraft_bot/src/" + arguments["file"]
+    python_filename = "minecraft_bot/src/" + arguments["file"]
 
     if arguments["check"]:
-        check_pep8_errors(python_file)
+        check_pep8_errors(python_filename)
+
+    if arguments["preview"]:
+        preview_autopep8(python_filename)
+
+    if arguments["refactor"]:
+        refactor_autopep8(python_filename)
+
