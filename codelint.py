3789f15325a8786041c95d08b980a946c91a3b17
karandesai-96 <karandesai_96@live.com>
2016-01-20 00:31:30 +0530

    codelint.py checks, previews, refactors all scipts in -a mode.
    Actions take place upon minecraft_bot and spockextras and all other
    sub directories.

--- codelint.py
+++ codelint.py
@@ -18,10 +18,10 @@ arg_parser.add_argument("-r", "--refactor", action="store_true",
                         help="refactors file passed as argument to resolve \
                              PEP8 errors, making changes in place.")
 
-arg_parser.add_argument("-a", "--all", action="store_true", help="applies \
-                        same action to all files in src directory, must be \
-                        used alongwith one of -c, -p, -r flags, if not, -r \
-                        will be default flag.")
+arg_parser.add_argument("-a", "--all", action="store_true", help="applies same \
+                        action to all files in src directory, must be used \
+                        alongwith one of -c, -p, -r flags, if not, -r will \
+                        be default flag.")
 
 arg_parser.add_argument("-i", "--interactive", action="store_true",
                         help="enables interactive mode for purpose.")
@@ -37,53 +37,56 @@ all_files = ["minecraft_bot/src/" + f + ".py" for f in
              spockextras.plugins.helpers.__all__] + \
             ["spockextras/" + f + ".py" for f in spockextras.__all__]
 
+all_dirs = ["minecraft_bot/src/", "spockextras/plugins/cores/",
+            "spockextras/plugins/helpers/", "spockextras/"]
+
 
 def check_pep8_errors(filename):
-    print "***** PEP8 errors in " + filename.split("/")[2] + " *****"
+    print "***** PEP8 errors in " + filename + " *****"
     os.system("pep8 " + filename)
     print "DONE !\n\n"
 
 
 def preview_autopep8(filename):
-    print "***** After refactoring " + filename.split("/")[2] + " *****"
-    os.system("autopep8 " + filename)
+    print "***** After refactoring " + filename + " *****"
+    os.system("autopep8 -a " + filename)
     print "DONE !\n\n"
 
 
 def refactor_autopep8(filename):
-    print "***** Refactoring " + filename.split("/")[2] + " *****"
+    print "***** Refactoring " + filename + " *****"
     os.system("autopep8 -i -a " + filename)
     print "DONE !\n\n"
 
 
 def check_pep8_errors_all():
-    directory = "minecraft_bot/src/"
-    all_filenames = [f for f in os.listdir(directory)
-                     if os.path.isfile(os.path.join(directory, f))]
+    for directory in all_dirs:
+        all_filenames = [f for f in os.listdir(directory)
+                         if os.path.isfile(os.path.join(directory, f))]
 
-    for filename in all_filenames:
-        if filename[-3:] == ".py":
-            check_pep8_errors(directory + filename)
+        for filename in all_filenames:
+            if filename[-3:] == ".py":
+                check_pep8_errors(directory + filename)
 
 
 def preview_autopep8_all():
-    directory = "minecraft_bot/src/"
-    all_filenames = [f for f in os.listdir(directory)
-                     if os.path.isfile(os.path.join(directory, f))]
+    for directory in all_dirs:
+        all_filenames = [f for f in os.listdir(directory)
+                         if os.path.isfile(os.path.join(directory, f))]
 
-    for filename in all_filenames:
-        if filename[-3:] == ".py":
-            preview_autopep8(directory + filename)
+        for filename in all_filenames:
+            if filename[-3:] == ".py":
+                preview_autopep8(directory + filename)
 
 
 def refactor_autopep8_all():
-    directory = "minecraft_bot/src/"
-    all_filenames = [f for f in os.listdir(directory)
-                     if os.path.isfile(os.path.join(directory, f))]
+    for directory in all_dirs:
+        all_filenames = [f for f in os.listdir(directory)
+                         if os.path.isfile(os.path.join(directory, f))]
 
-    for filename in all_filenames:
-        if filename[-3:] == ".py":
-            refactor_autopep8(directory + filename)
+        for filename in all_filenames:
+            if filename[-3:] == ".py":
+                refactor_autopep8(directory + filename)
 
 
 def start_interactive_mode():
@@ -92,7 +95,12 @@ def start_interactive_mode():
     new_task = "y"
 
     while new_task == "y":
-        filename = "minecraft_bot/src/" + raw_input("Enter filename: ")
+        filename = raw_input("Enter filename: ")
+        for a_file in all_files:
+            if a_file.split("/")[-1] == filename:
+                filename = a_file
+                break
+
         action = raw_input("Choose action [c/p/r]: ")
         if action == "c":
             check_pep8_errors(filename)
