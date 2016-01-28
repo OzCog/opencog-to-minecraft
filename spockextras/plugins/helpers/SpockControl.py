aa3c65b447f9e62a81ca328ed06fea44c212205c
Prabhjyot Singh Sodhi <prabhyotsingh95@gmail.com>
2016-01-28 22:34:16 +0530

    listen to client death update

--- spockextras/plugins/helpers/SpockControl.py
+++ spockextras/plugins/helpers/SpockControl.py
@@ -9,16 +9,16 @@ currently the ROS message is passed along unmodified. this may need to change la
 
 """
 
-import roslib; roslib.load_manifest('minecraft_bot')
+import roslib
+roslib.load_manifest('minecraft_bot')
 import rospy
 from minecraft_bot.msg import movement_msg, place_block_msg, mine_block_msg
 from minecraft_bot.msg import chunk_data_msg, chunk_bulk_msg, chunk_meta_msg, block_data_msg
 
 from minecraft_bot.msg import entity_msg, entity_exp_meta, entity_global_meta, entity_mob_meta
-from minecraft_bot.msg import entity_movement_meta, entity_object_meta, entity_painting_meta, entity_player_meta 
+from minecraft_bot.msg import entity_movement_meta, entity_object_meta, entity_painting_meta, entity_player_meta
 
 from minecraft_bot.msg import position_msg
-
 
 
 from spockbot import mcdata
@@ -35,16 +35,15 @@ class SpockControlCore:
         self.target = None
 
 
-
 @pl_announce('SpockControl')
 class SpockControlPlugin:
-    
+
     def __init__(self, ploader, settings):
-        
+
         self.event = ploader.requires('Event')
         self.msgr = ploader.requires('Messenger')
         self.net = ploader.requires('Net')
-        
+
         # the standard Spock plugin. necessary to track position updates
         # and any other state changes to the client from the Minecraft server
         self.clinfo = ploader.requires('ClientInfo')
@@ -63,48 +62,57 @@ class SpockControlPlugin:
         #ploader.reg_event_handler('ros_world_reset', self.sendWorldReset)
         ploader.reg_event_handler('ros_entity_data', self.sendEntityData)
 
-
-        #event handlers for all client related events
+        # event handlers for all client related events
         #ploader.reg_event_handler('cl_login_success', self.sendClientLogin)
         #ploader.reg_event_handler('cl_join_game', self.sendClientJoinGame)
         #ploader.reg_event_handler('cl_spawn_update', self.sendClientSpawnUpdate)
         #ploader.reg_event_handler('cl_health_update', self.sendClientHealthUpdate)
-        ploader.reg_event_handler('ros_position_update', self.sendClientPositionUpdate)
-        
+        ploader.reg_event_handler('client_death', self.sendClientDeathUpdate)
+        ploader.reg_event_handler(
+            'ros_position_update', self.sendClientPositionUpdate)
+
         self.core = SpockControlCore()
         ploader.provides('SpockControl', self.core)
-        
+
         self.initSpockControlNode()
 
-
     def initSpockControlNode(self):
-        
+
         rospy.init_node('spock_controller')
-	print("spock control node initialized")
-	
+        print("spock control node initialized")
+
         # subscribe to Spock related data streams from ROS
-	rospy.Subscriber('movement_data', movement_msg, self.moveTo, queue_size=1)
-	rospy.Subscriber('mine_block_data', mine_block_msg, self.mineBlock, queue_size=1)
-	rospy.Subscriber('place_block_data', place_block_msg, self.placeBlock, queue_size=1)
+        rospy.Subscriber(
+            'movement_data', movement_msg, self.moveTo, queue_size=1)
+        rospy.Subscriber(
+            'mine_block_data', mine_block_msg, self.mineBlock, queue_size=1)
+        rospy.Subscriber(
+            'place_block_data', place_block_msg, self.placeBlock, queue_size=1)
         #rospy.Subscriber('look_test', position_msg, self.lookTest, queue_size = 1)
 
         #self.pub_time =      rospy.Publisher('time_data', time_msg, queue_size = 1)
         #self.pub_dim =       rospy.Publisher('dimension_data', dim_msg, queue_size = 1)
-        self.pub_chunk =    rospy.Publisher('chunk_data', chunk_data_msg, queue_size = 1000)
-        self.pub_block =    rospy.Publisher('block_data', block_data_msg, queue_size = 1000)
-        self.pub_bulk =     rospy.Publisher('chunk_bulk', chunk_bulk_msg, queue_size = 1000)
+        self.pub_chunk = rospy.Publisher(
+            'chunk_data', chunk_data_msg, queue_size=1000)
+        self.pub_block = rospy.Publisher(
+            'block_data', block_data_msg, queue_size=1000)
+        self.pub_bulk = rospy.Publisher(
+            'chunk_bulk', chunk_bulk_msg, queue_size=1000)
         #self.pub_wstate =    rospy.Publisher('world_state', world_state_msg, queue_size = 1)
-        self.pub_entity =   rospy.Publisher('entity_data', entity_msg, queue_size = 100)
+        self.pub_entity = rospy.Publisher(
+            'entity_data', entity_msg, queue_size=100)
 
         #self.pub_clinfo_login = rospy.Publisher('client_login_data', position_msg, queue_size = 10)
         #self.pub_clinfo_join = rospy.Publisher('client_join_data', position_msg, queue_size = 10)
         #self.pub_clinfo_spawn = rospy.Publisher('client_spawn_data', position_msg, queue_size = 10)
         #self.pub_clinfo_health = rospy.Publisher('client_health_data', position_msg, queue_size = 10)
-        self.pub_clinfo_pos = rospy.Publisher('client_position_data', position_msg, queue_size = 100)
-    
+        self.pub_clinfo_pos = rospy.Publisher(
+            'client_position_data', position_msg, queue_size=100)
+
     def lookTest(self, data):
-    
-        # test to see what happens when we request pitch and yaw change from Minecraft server
+
+        # test to see what happens when we request pitch and yaw change from
+        # Minecraft server
         packet = {}
         packet['x'] = data.x
         packet['y'] = data.y
@@ -113,111 +121,98 @@ class SpockControlPlugin:
         packet['on_ground'] = True
         packet['yaw'] = data.yaw
         packet['pitch'] = data.pitch
-        
-        #everything is relative, for simplicity
+
+        # everything is relative, for simplicity
         #packet['flags'] = 0b11111
-        
+
         print "sending test packet for look only!"
         print packet
-    
+
         self.net.push_packet('PLAY>Player Position and Look', packet)
         #self.net.push_packet('PLAY>Player Position', packet)
 
-
-
-
-    ### ROS Subscriber callbacks simply pass data along to the Spock event handlers
+    # ROS Subscriber callbacks simply pass data along to the Spock event
+    # handlers
     def moveTo(self, data):
 
         self.event.emit('ros_moveto', data)
         print("emitting event ros_moveto")
 
-    
     def mineBlock(self, data):
 
         self.event.emit('ros_mineblock', data)
         print("emitting event ros_mineblock")
-    
-    
+
     def placeBlock(self, data):
 
         self.event.emit('ros_placeblock', data)
         print("emitting event ros_placeblock")
 
+    # Perception handlers. Invoke ROS Publishers
 
-    
-    ### Perception handlers. Invoke ROS Publishers
-    
-    #def sendTimeUpdate(self, name, data):
-    #    
+    # def sendTimeUpdate(self, name, data):
+    #
     #    msg = time_msg()
     #
     #    pub_time.publish(data)
-    
-    
-    #def sendNewDimension(self, data):
-    #    
+
+    # def sendNewDimension(self, data):
+    #
     #    pub_dim.publish(data)
-    
 
     def sendChunkData(self, name, data):
-        
+
         msg = chunk_data_msg()
         self.msgr.setMessage(msg, data)
-        
-        rospy.logdebug("published chunk message: loc: %d, %d", msg.chunk_x, msg.chunk_z)
+
+        rospy.logdebug(
+            "published chunk message: loc: %d, %d", msg.chunk_x, msg.chunk_z)
         self.pub_chunk.publish(msg)
-    
-    
+
     def sendChunkBulk(self, name, data):
-        
+
         msg = chunk_bulk_msg()
-        
+
         self.msgr.setMessage(msg, data)
 
         meta = []
         for i in range(len(data['metadata'])):
             meta.append(chunk_meta_msg())
             self.msgr.setMessage(meta[i], data['metadata'][i])
-        
+
         msg.metadata = meta
-        rospy.logdebug("published chunk bulk message, sky: %s, rostime: %s, mctime: %s", msg.sky_light, msg.ROStimestamp, msg.MCtimestamp)
-        
+        rospy.logdebug("published chunk bulk message, sky: %s, rostime: %s, mctime: %s",
+                       msg.sky_light, msg.ROStimestamp, msg.MCtimestamp)
+
         self.pub_bulk.publish(msg)
-    
-    
+
     def sendBlockUpdate(self, name, data):
-        
+
         msg = block_data_msg()
         self.msgr.setMessage(msg, data)
-        
-        rospy.logdebug("published block update: data, %d loc: %d, %d, %d", 
-                msg.data, msg.x, msg.y, msg.z)
+
+        rospy.logdebug("published block update: data, %d loc: %d, %d, %d",
+                       msg.data, msg.x, msg.y, msg.z)
         self.pub_block.publish(msg)
-    
-    
-    
-    #def sendWorldReset(self, name, data):
-    #    
+
+    # def sendWorldReset(self, name, data):
+    #
     #    pub_wstate.publish(data)
 
-
     def sendEntityData(self, name, data):
-        
+
         rospy.logdebug(data)
         msg = entity_msg()
         self.msgr.setMessage(msg, data)
 
         rospy.logdebug("published entity message: type: %d, uid: %d, loc: %d, %d, %d",
-                msg.type, msg.eid, msg.x, msg.y, msg.z)
+                       msg.type, msg.eid, msg.x, msg.y, msg.z)
         self.pub_entity.publish(msg)
 
-
     def sendClientLogin(self, name, data):
-        
+
         print "received client login"
         print data
-
 
     def sendClientJoinGame(self, name, data):
 
@@ -225,24 +220,26 @@ class SpockControlPlugin:
         print data
 
     def sendClientSpawnUpdate(self, name, data):
-        
+
         print "received client spawn"
         print data
 
-
     def sendClientHealthUpdate(self, name, data):
-        
+
         print "received client health update"
         print data
 
+    def sendClientDeathUpdate(self, name, data):
+
+        print "received client Death update"
+        print data
 
     def sendClientPositionUpdate(self, name, data):
-        
-        #print "received client position update"
-        #print data
-        
+
+        # print "received client position update"
+        # print data
+
         msg = position_msg()
         self.msgr.setMessage(msg, data)
-        
-        self.pub_clinfo_pos.publish(msg)
 
+        self.pub_clinfo_pos.publish(msg)
