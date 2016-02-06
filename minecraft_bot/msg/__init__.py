6d81ddb327632a0135568de2112a68622e7fb14f
akhisud <akhileshs.s4@gmail.com>
2016-02-06 23:35:01 +0530

    Create msg types to store inventory msgs and slot msgs

--- minecraft_bot/msg/__init__.py
+++ minecraft_bot/msg/__init__.py
@@ -5,4 +5,4 @@ __all__ = ["block_data_msg", "chunk_bulk_msg", "chunk_data_msg",
            "entity_mob_meta", "entity_movement_meta", "entity_msg",
            "entity_object_meta", "entity_painting_meta", "entity_player_meta",
            "map_block_msg", "map_block_multi_msg", "mine_block_msg",
-           "movement_msg", "place_block_msg", "position_msg", "vec3_msg"]
+           "movement_msg", "place_block_msg", "position_msg", "vec3_msg", "health_msg", "iventory_msg", "slot_msg", "slot_multi_msg"]
